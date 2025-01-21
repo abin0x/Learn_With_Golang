dca839530f19b72acb794a6ce8842e8b88dfdcf0
abin0x <mahmudulabin@gmail.com>
2025-01-21 20:16:09 +0600

    If condition add

---
+++ First Program/if_condition.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 2
+	if age > 18 {
+		fmt.Println("You are eligible to vote")
+	} else {
+		fmt.Println("You are not eligible to vote")
+	}
+
+}
